9f45dec8df7e6006e07683d171ca3ab4288455d0
Dan Tayari <kisa@tayari.co.il>
2025-01-08 17:52:54 +0200

    //meme editor improved functionality#

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -3,27 +3,8 @@
 const gElCanvas = document.querySelector('.meme-canvas')
 const gCtx = gElCanvas.getContext('2d')
 let gIsWriting = false
+let gIsFirstClick = true
 
-
-
-// check if the text area is clicked
-document.documentElement.addEventListener("click", (event) => {
-    const elLineText = document.querySelector('.line-text')
-    const isClickInside = elLineText.contains(event.target)
-    // console.log(event)
-    
-    if (!isClickInside) {
-        console.log('Clicked outside the line-text element')
-        toggleWritingMode(0, 0, false)
-    } else {
-        toggleWritingMode(0, 0, true)
-        // console.log(event.type)
-    }
-})
-
-document.querySelector('.line-text').addEventListener('keyup', () => [
-    setLineTxt(document.querySelector('.line-text').value)
-])
 
 function onInit() {
     getMeme()
@@ -32,14 +13,36 @@ function onInit() {
 
 }
 
+// check if the text area is clicked
+document.documentElement.addEventListener("click", (event) => {
+    const elLineText = document.querySelector('.line-text')
+    const isClickInside = elLineText.contains(event.target)
+    // console.log(event)
+    
+    if (!isClickInside && !gIsFirstClick) {
+        console.log('Clicked outside the line-text element')
+        toggleWritingMode(0, 0, false)
+    } else if(isClickInside) {
+        toggleWritingMode(0, 0, true)
+        // console.log(event.type)
+    }
+})
+
+document.querySelector('.line-text').addEventListener('keyup', () => [
+    setLineTxt(document.querySelector('.line-text').value,)
+])
+
+
 function renderMeme(lineText = 'Add Text Here') {
     drawImg()
     renderMemeLine(50, 40, getMemes()[0].lines[0].txt)
 }
 
 function renderMemeLine(x, y, lineText) {
-    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
-    // drawImg()
+    gCtx.textBaseline = 'top'
+    gCtx.textAlign = 'left'
+    gCtx.font = `${getMemes()[0].lines[0].size} ${getMemes()[0].lines[0].font}`
+    console.log(getMemes()[0].lines[0].size)
     gCtx.fillText(lineText, x, y)
 }
 
--- js/meme.service.js
+++ js/meme.service.js
@@ -1,18 +1,19 @@
 'use strict'
 
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]
-var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 let gMemes = []
 
 
-function getMeme(){
+function getMeme() {
     let meme = {
         selectedImgId: 5,
         selectedImgIdx: 0,
         lines: [
             {
-                txt: 'Hy',
-                size: 20,
+                txt: 'Add Text Here',
+                size: '20px',
+                font: 'Ariel',
                 color: 'red',
                 isWritingMode: false
             }
@@ -22,35 +23,48 @@ function getMeme(){
     console.log(gMemes)
 }
 
-function getMemes(){
+function getMemes() {
     return gMemes
 }
 
-function setLineTxt(txt, memeIdx = 0, lineIdx = 0){
+function setLineTxt(txt, memeIdx = 0, lineIdx = 0) {
     gMemes[memeIdx].lines[lineIdx].txt = txt
     document.querySelector('.text-area').value = txt
-    console.log('setLine:',gMemes[memeIdx].lines[lineIdx].txt)
+    console.log('setLine:', gMemes[memeIdx].lines[lineIdx].txt)
 }
 
-function toggleWritingMode(memeIdx, lineIdx, isClicked){
-    if(isClicked){
-    gMemes[memeIdx].lines[lineIdx].isWritingMode = true
-    console.log(gMemes[memeIdx].lines[lineIdx].isWritingMode)
-    createTextArea()
+function toggleWritingMode(memeIdx, lineIdx, isClicked) {
+    if (isClicked) {
+        gMemes[memeIdx].lines[lineIdx].isWritingMode = true
+        // console.log(gMemes[memeIdx].lines[lineIdx].isWritingMode)
+        if (!gIsFirstClick){ 
+            drawImg()
+        }
+        createTextArea(10, 30, gMemes[memeIdx].lines[lineIdx].txt)
+        if (gIsFirstClick) gIsFirstClick = false
     }
-    else{
+    else {
         gMemes[memeIdx].lines[lineIdx].isWritingMode = false
         console.log(gMemes[memeIdx].lines[lineIdx].isWritingMode)
+        renderMemeLine(10, 30, gMemes[memeIdx].lines[lineIdx].txt)
+        destroyTextArea()
     }
 }
 
-function createTextArea(x = 10 ,y= 30, text = 'Add Text Here'){
-    let textArea = document.createElement('textarea') // Create a new textarea element
-    textArea.style.position = 'absolute' // Set position to absolute
-    textArea.style.left = `${x}px` // Set the x position
-    textArea.style.top = `${y}px` // Set the y position
+function createTextArea(x = 10, y = 30, text) {
+    let textArea = document.createElement('textarea') 
+    textArea.style.position = 'absolute' 
+    textArea.style.left = `${x}px` 
+    textArea.style.top = `${y}px`
     textArea.style.background = 'transparent'
+    textArea.style.fontSize = gMemes[0].lines[0].size
+    textArea.style.font = gMemes[0].lines[0].font
     textArea.classList.add('text-area')
-    textArea.value = text // Set the initial text
+    textArea.value = text 
+    // console.log(text)
     document.body.appendChild(textArea)
+}
+
+function destroyTextArea() {
+    document.querySelector('.text-area').remove()
 }
